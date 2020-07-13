6b3b06c6d34049f13e16b53c5815bd1d445ab28b
QUY DUONG <quyduong2006@gmail.com>
2020-07-14 00:34:42 +0700

    Check connect stack with github

--- selemium/webdriver/Topic_Check_TestNG.java
+++ selemium/webdriver/Topic_Check_TestNG.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 
 public class Topic_Check_TestNG {
 	WebDriver driver;
-
+// check commit with slack
 	@BeforeClass
 	public void beforeClass() {
 		driver = new FirefoxDriver();
